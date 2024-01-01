ce697c09084334247855c2cf89276ea80f5f91c1
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2024-01-01 01:02:10 +0000

    removed redundant router

--- src/routes/apiRouter.ts
+++ src/routes/apiRouter.ts
@@ -1,8 +1,6 @@
 import { Router } from "express";
-import userRouter from "./userRouter";
 import applicationRouter from "./applicationRouter";
 const apiRouter = Router();
 
-apiRouter.use("/user", userRouter);
 apiRouter.use("/app", applicationRouter);
 export default apiRouter;
