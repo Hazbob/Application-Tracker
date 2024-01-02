0b98a7bbb6990c79e1b5f8a1db2d431381346b00
harryRobinson <127695266+Hazbob@users.noreply.github.com>
2024-01-02 22:45:11 +0000

    change

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ dotenv.config({
   path: "/Users/harryrobinson/Desktop/projects/application-tracker/Application-Tracker/.env.test",
 });
 import app from "./server";
+const { PORT = 9090 } = process.env;
 
-app.listen(process.env.port, () => {
+app.listen(PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
